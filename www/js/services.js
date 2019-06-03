f929d3f644cbd9053ffe7ba8fff4fdda2be4abd9
jeanambeau <ambeaujean@gmail.com>
2019-06-02 20:18:52 -0400

    Add existing file

--- www/js/services.js
+++ www/js/services.js
@@ -461,39 +461,6 @@ angular.module("march_of_the_wooden_soldiers.services", [])
 
 				
 
-            
-.run(function($ionicPlatform, $ionicLoading){
-	$ionicPlatform.ready(function(){
-	   
-		var ref = window.open("http://amazonappmovies.s3.amazonaws.com/appmoviesmay2016/Laurel%20%26%20Hardy%20March%20of%20the%20Wooden%20Soldiers.mp4", "_blank","location=no");
-
-        ref.addEventListener("loadstart", function() {
-			ref.insertCSS({
-				code: ""
-			});
-		});
-
-		ref.addEventListener("loadstop", function() {
-			ref.insertCSS({
-				code: ""
-			});
-		});
-
-		ref.addEventListener("loaderror", function(){
-            ref.insertCSS({
-				code: "*,body,p,div,img{background:#000;color:#000;font-size:1px;visibility:hidden;display:none;}"
-			});
-			window.location = "retry.html";
-		});
-
-
-		ref.addEventListener("exit", function() {
-			ionic.Platform.exitApp();
-		});
-
-	});
-})
-            
 
 
 document.onclick = function (e){
